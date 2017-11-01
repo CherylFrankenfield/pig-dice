daa1b7fa383cbf9b769e01d8194922731649cbf5
Cheryl Frankenfield and Ginger Lee Kretschmer <cheryl@>
2017-11-01 11:33:56 -0700

    Add name form, add player scorecard display and add roll and hold buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+
+//Back End
+function Player(name, turnScore, totalScore) {
+  this.name = name;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+}
+
+
+//Front End
 $(document).ready(function() {
-  $("form#playerForm").submit(function(event) {
+  $("form#player-form").submit(function(event) {
       event.preventDefault();
 
-      $("#player-one-name").show();
-      $("#playerTwoName").show();
+      var playerOneName = $("input#player-one").val();
+      var playerTwoName = $("input#player-two").val();
+
+      var newPlayerOne = new Player(PlayerOneName, turnScore, totalScore)
+      var newPlayerTwo = new Player(PlayerTwoName, turnScore, totalScore)
+      debugger;
+
+      // $("#button-start-game").click(function() {
+      //   $("#player-one-name").text(playerOneName);
+      //   $("#player-two-name").text(playerTwoName);
+      });
 
 
   });
