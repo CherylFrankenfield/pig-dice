7d0c7e22f6b67fceb8c7f126158a930b96003728
Cheryl Frankenfield and Ginger Lee Kretschmer <ginger@>
2017-11-01 14:07:27 -0700

    add ability to add player name

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,47 @@
 
 //Back End
-function Player(name, turnScore, totalScore) {
-  this.name = name;
-  this.turnScore = turnScore;
-  this.totalScore = totalScore;
+function Player(playerName) {
+  this.playerName = playerName;
+  this.turnScore;
+  this.totalScore;
 }
 
+function Game(player1, player2, diceRoll, totalGameScore) {
+  this.player1 = player1;
+  this.player2 = player2;
+  this.diceRoll;
+  this.totalGameScore;
+}
+
+var diceRoll = function() {
+  var roll = Math.floor((Math.random() * 6) + 1);
+  return roll;
+}
+
+// Player.prototype.setTurnScore = function() {
+//   if (this.diceRoll === 1) {
+//     this.turnScore = 0;
+//   } else { (index = 2; index <= 6; index++)
+//       if (this.diceroll === 2)
+//   }
+// });
+
+var player1 = new Player()
+var player2 = new Player()
 
 //Front End
 $(document).ready(function() {
   $("form#player-form").submit(function(event) {
       event.preventDefault();
 
-      var playerOneName = $("input#player-one").val();
-      var playerTwoName = $("input#player-two").val();
+      player1.playerName = $("input#player-one").val();
+      player2.playerName = $("input#player-two").val();
 
-      var newPlayerOne = new Player(PlayerOneName, turnScore, totalScore)
-      var newPlayerTwo = new Player(PlayerTwoName, turnScore, totalScore)
-      debugger;
-
-      // $("#button-start-game").click(function() {
-      //   $("#player-one-name").text(playerOneName);
-      //   $("#player-two-name").text(playerTwoName);
+      $("#button-start-game").click(function() {
+        $("#player-one-name").text(player1.playerName);
+        $("#player-two-name").text(player2.playerName);
       });
-
-
   });
+
+
 });
