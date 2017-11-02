87519a488927e45359146b3c565edf0f5359598b
Cheryl Frankenfield and Ginger Lee Kretschmer <ginger@>
2017-11-02 16:37:52 -0700

    update player object

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,13 @@
 //Back End
 function Player(name, score) { //player object constructor
   this.name = name;
-  this.score = [];
+  this.score = 0;
 }
 
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
   this.turn = 0;
-  this.newGame;
 }
 
 Game.prototype.switchTurn = function(){
@@ -24,13 +23,11 @@ var diceRoll = function() {
   return roll;
 }
 
-var playerArray = [];
-var rollArray = [];
-
 var add = function(a,b) {
   return a + b;
 }
 
+var rollArray = [];
 var player1 = new Player()
 var player2 = new Player()
 var newGame = new Game()
@@ -45,14 +42,17 @@ $(document).ready(function() {
       player2.name = $("input#player-two").val();
       $("#player-one-name").text(player1.name);
       $("#player-two-name").text(player2.name);
+
       newGame.turn = player1;
-      var turnScore = rollArray.reduce(add, 0);
+
+      // var turnScore = rollArray.reduce(add, 0);
+
       $("#roll-button").click(function(){
         var diceTemp = diceRoll();
         $("#current-dice-roll").text(diceTemp);
           if (diceTemp < 2) {
             rollArray.push(0);
-            $("#turn-score").text(turnScore);
+            $("#turn-score").text(rollArray.reduce(add, 0));
             alert("Your Turn is Over!")
             newGame.switchTurn();
           } else {
@@ -62,11 +62,11 @@ $(document).ready(function() {
       });
 
       $("#hold-button").click(function() {
-        $("#turn-score").text(turnScore);
-        player1.score.push(turnScore);
+        $("#turn-score").text(rollArray.reduce(add, 0));
+        player1.score += rollArray[0];
         $("#player-one-total-score").text(player1.score);
-
       });
+
     }); // end of play button click
   });
 });
